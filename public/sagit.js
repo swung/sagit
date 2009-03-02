56ce90dec82b2a4810f732e31d86ab06916df772
jbi <jbi@jbi.localdomain>
2009-03-03 00:50:33 +0800

    join shiphome and image with fadeout/in effect

--- public/sagit.js
+++ public/sagit.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-    $(".shiphome").draggable();
+    $(".shiphome").droppable({
+        drop: function(event,ui) {
+          $("<li>" + $(this).text() + ui.draggable.text() + "</li>").appendTo("#merge").fadeOut(500).fadeIn(1500);
+          ui.draggable.fadeOut(1200);
+        }
+      });
     $(".vmimage").draggable();
 });
