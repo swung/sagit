9b89a6823884ed188f477c48d8eda64259809668
swung <swung.cn@gmail.com>
2009-03-03 15:39:05 -0800

    sass

--- lib/sagit/helpers.rb
+++ lib/sagit/helpers.rb
@@ -3,7 +3,7 @@ Dir["#{File.dirname(__FILE__)}/helpers/*.rb"].each &method(:require)
 module Sagit
   module Helpers
     include Rendering
-
+    include PrettyOutput
 
 #include Rack::Utils
 #    alias :h :escape_html
